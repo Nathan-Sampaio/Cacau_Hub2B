597f0c2b6a76ff99b198934877702f5e9389eab8
Nathan-Sampaio <nathan.oliveira.sampaio@gmail.com>
2021-12-10 12:48:35 -0300

    Tratativa para status diferentes

---
+++ Dominio/Entidade/StatusPedido/StatusDiferente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidade.StatusPedido
+{
+    public class StatusDiferente
+    {
+        public string Status { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool Active { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Dominio/Interface/Servico/Status/IStatuService.cs
@@ -0,0 +1,14 @@
+﻿using Dominio.Entidade.StatusPedido;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interface.Servico.Status
+{
+    public interface IStatuService
+    {
+        Task<string> AdicionaStatusDiferentes(StatusPedidoCS status);
+    }
+}
